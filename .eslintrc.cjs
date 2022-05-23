94571354b1f233f6a34747dc6aa67e6b7aac1d5b
Andrew Brooke <andrewbrooke15@gmail.com>
2022-05-23 15:38:45 -0400

    Setup env and dependencies

---
+++ .eslintrc.cjs
@@ -0,0 +1,32 @@
+// eslint-disable-next-line no-undef
+module.exports = {
+    'env': {
+        'node': true,
+        'es2021': true,
+        'jest/globals': true
+    },
+    'extends': 'eslint:recommended',
+    'parserOptions': {
+        'ecmaVersion': 'latest',
+        'sourceType': 'module'
+    },
+    'plugins': ['jest'],
+    'rules': {
+        'indent': [
+            'error',
+            4
+        ],
+        'linebreak-style': [
+            'error',
+            'unix'
+        ],
+        'quotes': [
+            'error',
+            'single'
+        ],
+        'semi': [
+            'error',
+            'always'
+        ]
+    }
+};
